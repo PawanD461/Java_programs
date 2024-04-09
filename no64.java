ddd83d7fc247c5e8ade101ef36f11f686a3cd6fd
Pawandas461 <daspawan53785@gmail.com>
2024-04-09 11:08:37 +0530

    Recursion basic program practice

---
+++ no64.java
@@ -0,0 +1,23 @@
+// first occurence of an element in a array
+
+import java.util.*;
+public class no64{
+
+    public static int firstOccur(int arr[], int key, int i){
+        if(i == arr.length){
+            return -1;
+        }
+
+        if(arr[i] == key){
+            return i;
+        }
+
+        return firstOccur(arr, key , i+1);
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {4,5,7,1,8,0,2,3,9,6};
+        System.out.println(firstOccur(arr, 5, 0));
+        
+    }
+}
